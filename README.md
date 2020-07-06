b4969b46f6472f1ba4ee608964195fefe5e34a74
Varinder singh <varinder83singh@gmail.com>
2020-07-06 07:03:42 -0700

    add log

--- README.md
+++ README.md
@@ -11,3 +11,8 @@ https://opensource.zalando.com/zalenium/
 ```
 $ pytest test_1.py –slack_hook=https://hooks.slack.com/services/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 –slack_channel=general –slack_username=”V”
 ```
+
+```
+pytest --esend=True --euname="dbaecefcc9d01676292162c50adca8a4" --epwd="<key here>" --eto="varindernagra@gmail.com,varinder83singh@gmail.com" --esubject="Automation results" --esmtp="in-v3.mailjet.com"
+
+```
--- test_1.py
+++ test_1.py
@@ -1,8 +1,15 @@
 import unittest
 import os
 import time
+import logging
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
+logging.basicConfig(filename='test.log',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
 
 
 class HackerNewsSearchTest(unittest.TestCase):
@@ -17,6 +24,7 @@ class HackerNewsSearchTest(unittest.TestCase):
         self.browser = webdriver.Chrome()
 
     def test_hackernews_search_for_testdrivenio(self):
+        start_time = time.time()
         browser = self.browser
         browser.get('https://news.ycombinator.com')
         search_box = browser.find_element_by_name('q')
@@ -24,8 +32,10 @@ class HackerNewsSearchTest(unittest.TestCase):
         search_box.send_keys(Keys.RETURN)
         time.sleep(3)  # simulate long running test
         self.assertIn('testdriven.io', browser.page_source)
+        logging.info("--- %s seconds ---" % (time.time() - start_time))
 
     def test_hackernews_search_for_selenium(self):
+        start_time = time.time()
         browser = self.browser
         browser.get('https://news.ycombinator.com')
         search_box = browser.find_element_by_name('q')
@@ -33,8 +43,10 @@ class HackerNewsSearchTest(unittest.TestCase):
         search_box.send_keys(Keys.RETURN)
         time.sleep(3)  # simulate long running test
         self.assertIn('selenium', browser.page_source)
+        logging.info("--- %s seconds ---" % (time.time() - start_time))
 
     def test_hackernews_search_for_testdriven(self):
+        start_time = time.time()
         browser = self.browser
         browser.get('https://news.ycombinator.com')
         search_box = browser.find_element_by_name('q')
@@ -42,8 +54,10 @@ class HackerNewsSearchTest(unittest.TestCase):
         search_box.send_keys(Keys.RETURN)
         time.sleep(3)  # simulate long running test
         self.assertIn('testdriven', browser.page_source)
+        logging.info("--- %s seconds ---" % (time.time() - start_time))
 
     def test_hackernews_search_with_no_results(self):
+        start_time = time.time()
         browser = self.browser
         browser.get('https://news.ycombinator.com')
         search_box = browser.find_element_by_name('q')
@@ -51,7 +65,7 @@ class HackerNewsSearchTest(unittest.TestCase):
         search_box.send_keys(Keys.RETURN)
         time.sleep(3)  # simulate long running test
         self.assertNotIn('<div>', browser.page_source)
-        assert 2 == 1
+        logging.info("--- %s seconds ---" % (time.time() - start_time))
 
     def tearDown(self):
         self.browser.quit()  # quit vs close?
